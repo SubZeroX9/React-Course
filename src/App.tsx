a3950ddd604b1997ef209e8e01c81d921bc25d76
Rafael Azriaiev <rafael.azrv@gmail.com>
2025-12-11 18:12:27 +0200

    Added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <button onClick={() => setCount((count) => count + 1)}>
+    <button         
+    className="px-6 py-3 bg-blue-600 text-white rounded-xl text-lg font-semibold shadow-lg hover:bg-blue-700 active:scale-95 transition"
+    onClick={() => setCount((count) => count + 1)}>
       count is {count}
     </button>
   )
